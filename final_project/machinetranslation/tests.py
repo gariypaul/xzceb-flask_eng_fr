488ec4579c1c9dd6991607cf5283e08a32b00043
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2023-07-04 20:56:37 +0300

    Added unit tests for translation functions

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,18 @@
+import unittest
+from translator import english_to_french,french_to_english
+
+class TestTranslator(unittest.TestCase):
+    def test_englishToFrench(self):
+        english_text = 'Hello'
+        expected = 'Bonjour'
+        french_text = english_to_french(english_text)
+        self.assertEqual(french_text,expected)
+
+    def test_frenchToEnglish(self):
+        french_text = 'Bonjour'
+        expected = 'Hello'
+        english_text = french_to_english(french_text)
+        self.assertEqual(english_text,expected)
+
+if __name__=="__main__":
+    unittest.main()
